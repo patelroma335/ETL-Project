6719620452ea3e0c413dd1ba2a846863e9051cac
patelroma335 <63165329+patelroma335@users.noreply.github.com>
2020-07-09 14:19:02 -0700

    Add files via upload

---
+++ Gun_Violence_TableSchema.sql
@@ -0,0 +1,42 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "Mass_Shootings" (
+    "Incident_ID" int   NOT NULL,
+    "Incident_Date" date   NOT NULL,
+    "State" varchar(50)   NOT NULL,
+    "CityorCountry" varchar(100)   NOT NULL,
+    "Num_Killed" int   NOT NULL,
+    "Num_Injured" int   NOT NULL,
+    CONSTRAINT "pk_Mass_Shootings" PRIMARY KEY (
+        "Incident_ID"
+     )
+);
+
+CREATE TABLE "Accidental_Deaths" (
+    "Incident_ID" int   NOT NULL,
+    "Incident_Date" date   NOT NULL,
+    "State" varchar(50)   NOT NULL,
+    "CityorCountry" varchar(100)   NOT NULL,
+    "Num_Killed" int   NOT NULL,
+    "Num_Injured" int   NOT NULL,
+    CONSTRAINT "pk_Accidental_Deaths" PRIMARY KEY (
+        "Incident_ID"
+     )
+);
+
+CREATE TABLE "Accidental_Injuries" (
+    "Incident_ID" int   NOT NULL,
+    "Incident_Date" date   NOT NULL,
+    "State" varchar(50)   NOT NULL,
+    "CityorCountry" varchar(100)   NOT NULL,
+    "Num_Killed" int   NOT NULL,
+    "Num_Injured" int   NOT NULL,
+    CONSTRAINT "pk_Accidental_Injuries" PRIMARY KEY (
+        "Incident_ID"
+     )
+);
+
